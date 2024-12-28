ab6442af572d3bebd9f0cbb25edf933d322e52a2
Ameya Kale <ameya.k.kale@gmail.com>
2024-12-28 14:29:10 -0800

    Opened API for frontend

--- src/main/java/ameya/CV/CV/controller/CVController.java
+++ src/main/java/ameya/CV/CV/controller/CVController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "https://prerequisite-course-identifier-frontend.vercel.app/")
 public class CVController {
     @Autowired
     CVService cvService;
