7626bf4f3e5eb8c434e6635db6638fd275540214
Ameya Kale <ameya.k.kale@gmail.com>
2024-12-28 14:13:08 -0800

    Testing incoming requests

--- src/main/java/ameya/CV/CV/controller/CVController.java
+++ src/main/java/ameya/CV/CV/controller/CVController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CVController {
     @Autowired
     CVService cvService;
